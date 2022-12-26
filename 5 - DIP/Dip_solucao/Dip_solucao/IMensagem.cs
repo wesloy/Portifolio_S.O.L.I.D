ccb9babf0a54d79f71c9247392ca5b49cbd1fe82
Wesley Eloy <52499714+wesloy@users.noreply.github.com>
2022-12-26 17:24:15 -0300

    Delete 5 - DIP/Dip_solucao directory

--- 5 - DIP/Dip_solucao/Dip_solucao/IMensagem.cs
+++
@@ -1,8 +0,0 @@
-﻿
-    // Implementa o contrato que uma mensagem deve ter
-    // Não se preocupa com o meio que vai ser usado para entregar: E-mail, SMS, Push...
-    public interface IMensagem
-    {
-        void EnviarMensagem(string mensagem);
-    }
-
